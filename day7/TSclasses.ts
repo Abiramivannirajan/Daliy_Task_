567e0972a23d93170638723e71b466da6185540c
Abiramivannirajan <vabi4135@gmail.com>
2024-09-30 14:47:46 +0530

    size reduced

--- day7/TSclasses.ts
+++ day7/TSclasses.ts
@@ -1,4 +1,5 @@
-<<<<<<< HEAD
+
+
 
 // struture of class
 //A class can include
@@ -39,46 +40,4 @@ newperson.personid=105;
 newperson.personname="abi";
 newperson.personno=11;
 
-=======
-
-// struture of class
-//A class can include
-//constructor
-//properties
-//methods
-class employee1{
-    name: string="abi";
-
-}
- const employee= new employee1();
- console.log(employee.name);
-
- //The constructor method is a special method:
-
-//It has to have the exact name "constructor"
-//It is executed automatically when a new object is created
-//It is used to initialize object properties
-
-//example
-class  person1
-{
-   personid:number;//class variable
-   personname:string;
-   personno:number;
-
-//method
-display():void{
-    console.log(this.personid);
-    console.log(this.personname);
-    console.log(this.personno);
-    
-}
-
-}
-var newperson= new person1();// object creation
-newperson.personid=105;
-newperson.personname="abi";
-newperson.personno=11;
-
->>>>>>> 52e1289d2c1008492f730f02ce3c50229263bb19
 newperson.display();
